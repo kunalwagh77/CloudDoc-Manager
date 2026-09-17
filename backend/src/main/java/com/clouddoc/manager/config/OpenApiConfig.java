590c0e58a33600bc894a3531c4c601cd9eaf22cc
Kunal Wagh <kunalwagh901@gmail.com>
2026-09-18 00:16:35 +0530

    Bypass reflection scanning in OpenApiConfig

--- backend/src/main/java/com/clouddoc/manager/config/OpenApiConfig.java
+++ backend/src/main/java/com/clouddoc/manager/config/OpenApiConfig.java
@@ -13,7 +13,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("CloudDoc Manager API")
-                        .version("1.0.0")
-                        .description("CloudDoc Backend Documentation"));
+                        .version("1.0")
+                        .description("API Documentation"));
     }
 }
