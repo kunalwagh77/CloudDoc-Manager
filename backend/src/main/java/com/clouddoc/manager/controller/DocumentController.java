0fe60cc44ce8eb9bdd5a6e555a42eeb568275736
Kunal Wagh <kunalwagh901@gmail.com>
2026-09-18 01:12:28 +0530

    Fix import paths matching main package structure

--- backend/src/main/java/com/clouddoc/manager/controller/DocumentController.java
+++ backend/src/main/java/com/clouddoc/manager/controller/DocumentController.java
@@ -1,7 +1,7 @@
 package com.clouddoc.manager.controller;
 
-import com.clouddoc.manager.model.*;
-import com.clouddoc.manager.repository.*;
+import com.clouddoc.model.Document;
+import com.clouddoc.repository.DocumentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
