e2770dc5d10e6afa392a0eb28fdec8b40d217fc9
Kunal Wagh <kunalwagh901@gmail.com>
2026-09-17 23:43:37 +0530

    Create controller and config classes for Swagger and Root endpoints

---
+++ backend/src/main/java/com/clouddoc/manager/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.clouddoc.manager.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://clouddoc-manager-mz7r.onrender.com").description("Production Server"),
+                        new Server().url("http://localhost:8080").description("Local Server")
+                ));
+    }
+}
